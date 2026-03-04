1d32dfcfc045bfb4d85adc47a8449bc5b67bada3
Kartik7615 <itankarkv@rknec.edu>
2026-03-04 23:47:19 +0530

    UI-project

---
+++ UI-project/src/components/Section1/Page1Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LeftContent from './LeftContent'
+import RightContent from './RightContent'
+
+const Page1Content = () => {
+  return (
+    <div className='py-10 flex items-center gap-10 justify-between h-[90vh] px-18'>
+      <LeftContent />
+      <RightContent />
+    </div>
+  )
+}
+
+export default Page1Content
